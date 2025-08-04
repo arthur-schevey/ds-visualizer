25f158f8f6437b5e78e67689c7a3e9e109809f76
Arthur Schevey <arthur.schevey@gmail.com>
2025-08-03 23:51:33 -0400

    SEO: Use absolute urls for OpenGraph

--- src/apps/graph/GraphPage.tsx
+++ src/apps/graph/GraphPage.tsx
@@ -6,7 +6,7 @@ const GraphPage = () => {
       <title>Graph Builder — DS Chef</title>
       <meta name="description" content={"Build and explore graphs interactively with DS Chef. Export test cases as JSON or edge lists and use them to develop or debug your algorithms."} />
       <meta property="og:title" content={"Graph Builder — DS Chef"} />
-      <meta property="og:image" content={"/public/images/graph-demo.png"} />
+      <meta property="og:image" content={"https://dschef.com/images/graph-demo.png"} />
       <GraphFlow/>
     </>
   )
--- src/apps/tree/TreePage.tsx
+++ src/apps/tree/TreePage.tsx
@@ -6,7 +6,7 @@ const TreePage = () => {
       <title>Binary Tree Builder — DS Chef</title>
       <meta name="description" content={"DS Chef’s Binary Tree Builder lets you create trees visually and export them in LeetCode format for algorithm practice, debugging, or teaching."} />
       <meta property="og:title" content={"Binary Tree Builder — DS Chef"} />
-      <meta property="og:image" content={"/public/images/tree-demo.png"} />
+      <meta property="og:image" content={"https://dschef.com/images/tree-demo.png"} />
       <TreeFlow />
     </>
   );
